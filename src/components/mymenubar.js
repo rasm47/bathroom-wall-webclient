a217be3b4e093de4c55ad905f06be0c054db793f
rasm47 <16821766+rasm47@users.noreply.github.com>
2020-12-23 16:47:03 +0200

    Enable showing login status on the top bar

--- src/components/mymenubar.js
+++ src/components/mymenubar.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import {Toolbar, AppBar, IconButton} from '@material-ui/core';
-import {Menu as MenuIcon} from '@material-ui/icons';
+import {Toolbar, AppBar, IconButton, Typography, Box} from '@material-ui/core';
+import {Menu as MenuIcon, AccountCircle as AccountCircleIcon} from '@material-ui/icons';
 
 export default function MyMenuBar(props) {
+  const showLogin = props.showLogin;
+  
   return (
-    <React.Fragment>
+    <div style={{flexGrow: 1}}>
     <AppBar position="fixed">
-      <Toolbar>
-        <IconButton onClick={props.homeClick}>
+      <Toolbar >
+        <IconButton edge="start" color="inherit" onClick={props.homeClick}>
           <MenuIcon />
         </IconButton>
+        <Typography color="inherit" variant="h5" style={{flexGrow: 1}}>Title text</Typography>
+        {showLogin && (
+          <AccountCircleIcon color="inherit" edge="end"/>
+          )}
+        <Typography color="inherit" >{showLogin ? "logged in" : "not logged in"}</Typography>
+        
       </Toolbar>
       
     </AppBar>
     <Toolbar />
-    </React.Fragment>
+    </div>
   );
 }
